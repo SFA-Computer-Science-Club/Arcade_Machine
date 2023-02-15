02aa4b096a5afe7adf046a7fe4a24be653186f84
gooseapple5 <32470858+gooseapple5@users.noreply.github.com>
2023-02-15 16:57:54 -0600

    block class
    added block class and functions

---
+++ data/components/block.py
@@ -0,0 +1,17 @@
+#Class for a block, contains update functions/draw functions
+
+import pygame
+from .. import prepare, tools, logging
+
+class Block():
+    def __init__(self, x, y, name, image, rect):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.image = image
+        self.rect = rect
+
+    def draw(self):
+        prepare._screen.blit(self.image, (self.x,self.y))
+
+    
--- data/components/world.py
+++ data/components/world.py
@@ -3,12 +3,41 @@ import sys
 import pygame as pg
 from csv import reader
 from .. import prepare, tools, logging
+from . import block
 
 class WorldMap(object):
     def __init__(self):
         self.name = prepare.testMap2
         self.loaded = prepare.loaded
         self.scrolling = False
+
+        #Create all of the objects
+        self.mapOneObjTable = []
+
+        with open(prepare.testMap, 'r') as read_obj:
+            csv_reader = prepare.csv.reader(read_obj)
+            for rowIndex, row in enumerate(csv_reader):
+                for columnIndex, column in enumerate(row):
+                        x = columnIndex * 64
+                        y = rowIndex * 64
+                        if column == '1':
+                            dirtBlock = block.Block(x,y,"dirt_block",prepare.dirtTexture,prepare.dirtTexture.get_rect().move(x,y))
+                            self.mapOneObjTable.append(dirtBlock)
+                        elif column == '2':
+                            goldStoneBlock = block.Block(x,y,"gold_stone_block",prepare.goldStoneTexture,prepare.goldStoneTexture.get_rect().move(x,y))
+                            self.mapOneObjTable.append(goldStoneBlock)
+                        elif column == '3':
+                            grassBlock = block.Block(x,y,"grass_block",prepare.grassTexture,prepare.grassTexture.get_rect().move(x,y))
+                            self.mapOneObjTable.append(grassBlock)
+                        elif column == '4':
+                            sfaBlock = block.Block(x,y,"sfa_block",prepare.sfaCubeTexture,prepare.sfaCubeTexture.get_rect().move(x,y))
+                            self.mapOneObjTable.append(sfaBlock)     
+                        elif column == '5':
+                            stoneBlock = block.Block(x,y,"stone_block",prepare.stoneTexture,prepare.stoneTexture.get_rect().move(x,y))
+                            self.mapOneObjTable.append(stoneBlock)
+                        elif column == '6':
+                            brickBlock = block.Block(x,y,"brick_block",prepare.brickBlockTexture,prepare.brickBlockTexture.get_rect().move(x,y))
+                            self.mapOneObjTable.append(brickBlock)
 
     def load(self, world_name):
         """Load world given a world_name."""
@@ -21,15 +50,8 @@ class WorldMap(object):
 
         # pass the file object to reader() to get the reader object
         # Iterate over each row in the csv using reader object
-        for rowIndex, row in enumerate(prepare.mapOneTable):
-            for columnIndex, column in enumerate(row):
-                x = columnIndex * 64
-                y = rowIndex * 64
-                if prepare.mapOneTable[rowIndex][columnIndex] == -1:
-                    continue
-                else:
-                    # this means that it isn't an air block so just put what value it is in the array
-                    prepare._screen.blit(prepare.mapOneTable[rowIndex][columnIndex], (x,y))
+        for object in self.mapOneObjTable:
+            object.draw()
     
     def update(self, now, player):
         self.load(self.name)
@@ -40,13 +62,12 @@ class WorldMap(object):
 
     def collision(self, player):
         didCollide = False
-        for rowIndex, row in enumerate(prepare.mapOneRectTable):
+        for object in self.mapOneObjTable:
             
             # Check if player's current coordinates are inside of each rect in mapOneRectTable\
-            rectObject = prepare.mapOneRectTable[rowIndex]
-            if player.rect.colliderect(rectObject):
+            if player.rect.colliderect(object.rect):
                 
-                player.collide(rectObject)
+                player.collide(object.rect)
                 
                 didCollide = True
         if (didCollide == False):
